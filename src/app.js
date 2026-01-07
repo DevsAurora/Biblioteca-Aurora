6d982aa314d4375a97a637e94211451edbbbdae2
Th1y <tiago.pinheiro.git@gmail.com>
2026-01-07 17:17:48 -0300

    Criar tabelas Livros e Usuario

--- src/app.js
+++ src/app.js
@@ -4,20 +4,22 @@ const port = process.env.PORT || 3000;
 
 const path = require('path'); 
 
-// Middleware para interpretar requisições com corpo em JSON
-app.use(express.json());
-
 // Configuração do motor de templates
 app.set('view engine', 'ejs'); 
 // Define o diretório onde ficam as views (templates EJS)
 app.set('views', path.join(__dirname, 'views'));
 
+// Middleware para interpretar requisições com corpo em JSON
+app.use(express.json());
+// Middleware para interpretar requisições com corpo em URL-encoded
+app.use(express.urlencoded({ extended: true }));
 // Middleware para servir arquivos estáticos (CSS, JS, imagens)
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Rotas principais da aplicação
 const homeRoutes = require('./routes/homeRoutes');
 const livroRoutes = require('./routes/livroRoutes');
+
 // Usa as rotas definidas
 app.use('/', homeRoutes);
 app.use('/livros', livroRoutes);
