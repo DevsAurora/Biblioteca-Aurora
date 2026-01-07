bbe14f63d32312d46b9a9cf3e9c77ed8c23e94ce
Tiago <tiago.pinheiro.git@gmail.com>
2026-01-06 21:32:55 -0300

    Revert "Configurar  porta dinâmica para uso no Railway"

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
 const express = require('express'); 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 const path = require('path'); 
-
-// Middleware para interpretar requisições com corpo em JSON
-app.use(express.json());
 
 // Configuração do motor de templates
 app.set('view engine', 'ejs'); 
@@ -17,10 +14,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Rotas principais da aplicação
 const homeRoutes = require('./routes/homeRoutes');
-const livroRoutes = require('./routes/livroRoutes');
+
 // Usa as rotas definidas
 app.use('/', homeRoutes);
-app.use('/livros', livroRoutes);
 
 // Inicializa o servidor
 app.listen(port, () => {
